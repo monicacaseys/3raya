c6d970d9e649eb5611b02ec2a5bec915b28db45f
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-04-27 12:23:32 +0200

    Update juego.js

--- juego.js
+++ juego.js
@@ -1,4 +1,5 @@
 let symbol = "$"; // símbolo que se dibujará al hacer clic
+let gameOver = false; // indica si el juego ha terminado
 
 function highlightCell(cellId) {
   let cell = document.getElementById(cellId);
@@ -10,68 +11,68 @@ function unhighlightCell(cellId) {
   cell.style.border = "";
 }
 
-/* function drawSymbol(cellId) {
-  let cell = document.getElementById(cellId);
-  if (cell.innerHTML === "") {
-    cell.innerHTML = symbol;
-    checkWin();
-  }
-} */
-
-const squares = document.querySelectorAll(".square");
-let symbols = ["", "", "", "", "", "", "", "", ""];
-
-squares.forEach(square => {
-  square.addEventListener("click", () => {
-    const index = square.id;
-    if (symbols[index] === "") {
-      square.textContent = "$";
-      symbols[index] = "$";
+function drawSymbol(cellId) {
+  if (!gameOver) {
+    let cell = document.getElementById(cellId);
+    if (cell.innerHTML === "") {
+      cell.innerHTML = symbol;
       checkWin();
     }
-  });
-});
+  }
+}
 
 function checkWin() {
-  const rows = [    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-  ];
-  const columns = [    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-  ];
-  const diagonals = [    [0, 4, 8],
-    [2, 4, 6],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-  ];
- // Comprobar filas
- for (let i = 0; i < 9; i += 3) {
-  if (symbols[i] !== '' && symbols[i] === symbols[i+1] && symbols[i] === symbols[i+2]) {
-    alert('¡Has ganado!');
-    return;
+  let cells = document.querySelectorAll("[id^='cell']");
+
+  // Revisar si hay 3 celdas consecutivas con el mismo símbolo
+  for (let i = 0; i < cells.length; i++) {
+    let currentSymbol = cells[i].innerHTML;
+    if (currentSymbol !== "") {
+      let row = Math.floor(i / 3);
+      let col = i % 3;
+
+      // Revisar fila
+      if (cells[row * 3].innerHTML === currentSymbol &&
+        cells[row * 3 + 1].innerHTML === currentSymbol &&
+        cells[row * 3 + 2].innerHTML === currentSymbol) {
+        
+        alert("¡Ganaste!");
+        gameOver = true;
+        return;
+      }
+
+      // Revisar columna
+      if (cells[col].innerHTML === currentSymbol &&
+        cells[col + 3].innerHTML === currentSymbol &&
+        cells[col + 6].innerHTML === currentSymbol) {
+        
+        alert("¡Ganaste!");
+        gameOver = true;
+        return;
+      }
+
+      // Revisar diagonal principal
+      if (row === col &&
+        cells[0].innerHTML === currentSymbol &&
+        cells[4].innerHTML === currentSymbol &&
+        cells[8].innerHTML === currentSymbol) {
+    
+        alert("¡Ganaste!");
+        gameOver = true;
+        return;
+      }
+
+      // Revisar diagonal secundaria
+      if (row + col === 2 &&
+        cells[2].innerHTML === currentSymbol &&
+        cells[4].innerHTML === currentSymbol &&
+        cells[6].innerHTML === currentSymbol) {
+        
+        alert("¡Ganaste!");
+        gameOver = true;
+        return;
+      }
+    }
   }
 }
 
-// Comprobar columnas
-for (let i = 0; i < 3; i++) {
-  if (symbols[i] !== '' && symbols[i] === symbols[i+3] && symbols[i] === symbols[i+6]) {
-    alert('¡Has ganado!');
-    return;
-  }
-}
-
-// Comprobar diagonal principal
-if (symbols[0] !== '' && symbols[0] === symbols[4] && symbols[0] === symbols[8]) {
-  alert('¡Has ganado!');
-  return;
-}
-
-// Comprobar diagonal secundaria
-if (symbols[2] !== '' && symbols[2] === symbols[4] && symbols[2] === symbols[6]) {
-  alert('¡Has ganado!');
-  return;
-}
-}
